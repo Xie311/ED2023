9a084d3e6ba0e51ea7ea1123120bc4da801e8ea9
x311 <xielina311@163.com>
2025-07-27 01:40:53 +0800

    修理好EIDE

--- 25_EE/ED_Trail/UserCode/Upper/Upper_Debug/upper_debug.h
+++ 25_EE/ED_Trail/UserCode/Upper/Upper_Debug/upper_debug.h
@@ -1,7 +1,17 @@
+/*
+ * @Author: X311
+ * @Date: 2025-07-27 00:10:26
+ * @LastEditors: X311 
+ * @LastEditTime: 2025-07-27 01:39:30
+ * @FilePath: \ED_Trail\UserCode\Upper\Upper_Debug\Upper_Debug.h
+ * @Brief: 
+ * 
+ * Copyright (c) 2025 by X311, All Rights Reserved. 
+ */
 #ifndef __UPPER_DEBUG_H
 #define __UPPER_DEBUG_H
 
-#include "Upperstart.h"
+#include "Chassis_start.h"
 
 void Upper_Debug_TaskStart(void);
 void Upper_Debug_Task(void *argument);
